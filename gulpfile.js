48a92dfb3babc6aafbfa2a0886eaf8432c86c24e
Quill <seriouslyquill@gmail.com>
2017-11-22 01:20:44 -0800

    added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const runSequence = require('run-sequence')
+
+let session = null
+
+gulp.task('babel', () => gulp
+    .src(['src/google.js'])
+    .pipe(babel({
+        presets: [
+            'es2015'
+        ],
+        plugins: [
+            'syntax-async-functions',
+            'transform-regenerator',
+            'transform-async-to-generator',
+            'transform-runtime'
+        ]
+    }))
+    .pipe(gulp.dest('bin')))
+
+/* CLI tasks */
+gulp.task('default', callback => {
+    runSequence('babel', callback)
+})
+gulp.task('start', callback => {
+    runSequence('babel', callback)
+})
